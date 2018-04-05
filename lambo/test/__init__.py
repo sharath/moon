d576b4e216946eaac2655fc12bb8428e6780bcd0
sharath <sharathramkumar@gmail.com>
2018-04-05 18:44:18 -0400

    actual codebase

---
+++ lambo/test/__init__.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class LamboTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(1, 1)
---
+++ moon/test/PredictorTest.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class PredictorTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(1, 1)
