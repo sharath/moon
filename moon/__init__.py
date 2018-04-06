af652cd5615be91259130d746efca0e81529cccb
sharath <sharathramkumar@gmail.com>
2018-04-06 00:46:22 -0400

    use scipy since i suck

--- moon/__init__.py
+++ moon/__init__.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import scipy.interpolate
 
 class Predictor:
     def __init__(self, x, y):
@@ -7,21 +7,7 @@ class Predictor:
         self.y = y
 
     def lagrange_interpolate(self, px):
-        # compute barycentric weights
-        w = np.zeros((1, len(self.x)))
-        for i in range(len(self.x)):
-            prod = np.float64(1)
-            for j in self.x:
-                if self.x[i] != j:
-                    prod *= self.x[j] - self.x[i]
-            w[i] = 1.0 / prod
-        # evaluation at px
-        pxn = 0
-        pxd = 0
-        for i in range(len(self.x)):
-            pxn += (w[i] * self.y[i]) / (px - (self.x[i]))
-            pxd += (w[i]) / (px - (self.x[i]))
-        return pxn / pxd
+        return scipy.interpolate.BarycentricInterpolator(self.x, self.y)(px)
 
 
 class Sanitizer:
